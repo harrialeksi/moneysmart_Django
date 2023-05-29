86be739518db360d4f58a0c2d679d3a79523f6b1
superbaby81230 <superbaby81230@gmail.com>
2023-05-29 23:05:32 +0300

    Modified cards router

--- static/js/main.js
+++ static/js/main.js
@@ -1,43 +1,61 @@
-$(document).ready(function() {
-	$('.more-det-btn').click(function() {
-		$('.more-det-content').toggleClass('active-det');
-		$('.less-con').toggleClass('active');
-		$('.more-con').toggleClass('active');
+const APP_DOMAIN = "http://localhost:8000";
 
-        
-	});
-});
-// ----------------input type-toggle-----
-$(document).ready(function() {
-	$('.icon-show').click(function() {
-	  $('#password').attr('type', 'text');
-	  $(this).css('display', 'none');
-	  $('.icon-hide').css('display', 'block');
-	});
-	$('.icon-hide').click(function() {
-	  $('#password').attr('type', 'password');
-	  $(this).css('display', 'none');
-	  $('.icon-show').css('display', 'block');
-	});
+$(document).ready(function () {
+  $(".more-det-btn").click(function () {
+    $(".more-det-content").toggleClass("active-det");
+    $(".less-con").toggleClass("active");
+    $(".more-con").toggleClass("active");
   });
-  
-// ---navbar-mbl------
-$(document).ready(function() {
-	$('.menu').click(function() {
-		var submenu = $(this).find('.submenu');
-		var icon = $(this).find('i');
-		$('.submenu').not(submenu).slideUp();
-		submenu.slideToggle();
-		icon.toggleClass('up');
-	});
+});
 
+// ----------------input type-toggle-----
+$(document).ready(function () {
+  $(".icon-show").click(function () {
+    $("#password").attr("type", "text");
+    $(this).css("display", "none");
+    $(".icon-hide").css("display", "block");
+  });
+  $(".icon-hide").click(function () {
+    $("#password").attr("type", "password");
+    $(this).css("display", "none");
+    $(".icon-show").css("display", "block");
+  });
+});
+
+// ---navbar-mbl------
+$(document).ready(function () {
+  $(".menu").click(function () {
+    var submenu = $(this).find(".submenu");
+    var icon = $(this).find("i");
+    $(".submenu").not(submenu).slideUp();
+    submenu.slideToggle();
+    icon.toggleClass("up");
+  });
+
+  $(".toggler").click(function () {
+    $(".mbl-nav").show();
+    $(".toggler").hide();
+  });
+  $(".close-icon").click(function () {
+    $(".mbl-nav").hide();
+    $(".toggler").show();
+  });
+});
+
+// ---card-provider select------
+$(document).ready(function () {
+  $(".provider").click(function () {
+    const currentUrl = window.location.href;
+	provider = $(this).val();
+
+    // Create a regular expression to match the "provider" query parameter    
+    const regex = /(\?|&)provider=\d+(&|$)/;
+    let i = currentUrl.search(regex);
+    let newUrl = "";
+    if (i < 0) newUrl = currentUrl + "?provider=" + provider;
+    // Replace the "provider" query parameter with the new value
+    else newUrl = currentUrl.replace(regex, `$1provider=${provider}$2`);
 	
-	$('.toggler').click(function() {
-		$('.mbl-nav').show();
-		$('.toggler').hide();
-	  });
-	  $('.close-icon').click(function() {
-		$('.mbl-nav').hide();
-		$('.toggler').show();
-	  });
+    window.location.replace(newUrl);
+  });
 });
