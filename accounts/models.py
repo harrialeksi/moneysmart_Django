8577031f1ded97853ca555e3a85da7ef2a6045c8
shinyhawk <hawkpeter0324@gmail.com>
2023-05-30 14:03:30 +0300

    accountsapp

---
+++ accounts/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Account(models.Model):
+    title = models.CharField(max_length=30, null=True)
+    category = models.CharField(max_length=100, null=True)
+    provider = models.ForeignKey(
+        "Provider", null=True, on_delete=models.PROTECT, related_name='provider')
+    image = models.CharField(max_length=200, null=True)
+    disclosure = models.CharField(max_length=30, null=True)
+    execlusive = models.CharField(max_length=20, null=True)
+    badge_label = models.CharField(max_length=20, null=True)
+    badge_primary = models.CharField(max_length=20, null=True)
+    snippet = models.TextField(null=True)
+    snippet_img = models.CharField(max_length=200, null=True)
+    promotion = models.TextField(null=True)
+    keyfeatures = models.TextField(null=True)
+    annualinterest = models.TextField(null=True)
+    incomeequirement = models.TextField(null=True)
+    cardassociation = models.TextField(null=True)
+    wirelesspayment = models.TextField(null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class AccountUsp(models.Model):
+    dd = models.CharField(max_length=30)
+    dt = models.CharField(max_length=200)
+    account = models.ForeignKey("Account", related_name='account_usp', on_delete=models.CASCADE)
+
+class Provider(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    slug = models.CharField(max_length=100, null=True)
+    doc = models.TextField(null=True)
--- backend/settings.py
+++ backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'cards',
     'loans',
     'insurance',
+    'accounts',
 ]
 
 MIDDLEWARE = [
